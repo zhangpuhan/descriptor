e0637adeb1cfbfb524e95da7b59154edbd412311
Puhan Zhang <pz4ee@virginia.edu>
2019-02-14 23:30:25 -0500

    021420192330

--- read_file.py
+++ read_file.py
@@ -19,7 +19,7 @@ class Aev:
                     self.filenames.append(path + "/" + filename)
         self.filenames = natsort.natsorted(self.filenames)
         
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device('cpu')
         print('Using device:', self.device)
         if self.device.type == 'cuda':
             print(torch.cuda.get_device_name(0))
